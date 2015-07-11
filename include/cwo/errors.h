a1f39a35bed4af7b482765136a66cc3d746dc6fa
Felix Palmen <felix@palmen-it.de>
2015-07-11 12:29:02 +0200

    all kinds of error codes for cwint_alloc()

--- include/cwo/errors.h
+++ include/cwo/errors.h
@@ -5,6 +5,9 @@
 
 #define CWOERR_NOMEM -1	    /* out of memory */
 #define CWOERR_NOENT -2	    /* entry not found */
+#define CWOERR_INVARG -3    /* invalid argument */
+#define CWOERR_NULLARG -4   /* null argument */
+#define CWOERR_INCARG -5    /* incompatible type of argument */
 
 #endif
 
--- src/cwo/internal/utils.c
+++ src/cwo/internal/utils.c
@@ -5,6 +5,17 @@ int
 cwoint_alloc(void *ptr_object, size_t size)
 {
     void **object = ptr_object;
+
+    if (size <= 0)
+    {
+	return CWOERR_INVARG;
+    }
+
+    if (!object)
+    {
+	return CWOERR_NULL;
+    }
+
     *object = malloc(size);
     if (*object)
     {
