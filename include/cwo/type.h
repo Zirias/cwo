427f774fae22520eeb90dc323e9599b3bb958693
Felix Palmen <felix@palmen-it.de>
2015-07-13 11:41:32 +0200

    basic typesystem: next iteration, still defunct

--- include/cwo/type.h
+++ include/cwo/type.h
@@ -4,6 +4,11 @@
 #include <cwo/common.h>
 #include <cwo/string.h>
 
+typedef int (*cwo_MethodCall)();
+typedef void *(*cwo_PropertyGet)(void *object);
+typedef void (*cwo_PropertySet)(void *object, void *value);
+typedef void (*cwo_Destructor)(void *object);
+
 struct cwo_Method_s;
 typedef struct cwo_Method_s cwo_Method;
 
@@ -17,6 +22,21 @@ struct cwo_Type_s;
 typedef struct cwo_Type_s cwo_Type;
 
 DECLEXPORT int
+cwo_Method_create(cwo_Method **self, const cwo_String *name,
+		cwo_MethodCall call, int numArgs, ...);
+
+DECLEXPORT void
+cwo_Method_destroy(cwo_Method *self);
+
+DECLEXPORT int
+cwo_Property_create(cwo_Property **self, const cwo_String *name,
+		const cwo_Type *type,
+		cwo_PropertyGet get, cwo_PropertySet set);
+
+DECLEXPORT void
+cwo_Property_destroy(cwo_Property *self);
+
+DECLEXPORT int
 cwo_Type_register(const cwo_Type **type,
 	const cwo_String *name,
 	const cwo_Type *base,
@@ -26,12 +46,20 @@ DECLEXPORT int
 cwo_Type_destroyInstance(const cwo_Type *self, void *instance);
 
 #ifdef CWO_NSIMPORT
+#define MethodCall cwo_MethodCall
+#define PropertyGet cwo_PropertyGet
+#define PropertySet cwo_PropertySet
+#define Destructor cwo_Destructor
 #define Method cwo_Method
 #define Property cwo_Property
 #define TypeDescriptor cwo_TypeDescriptor
 #define Type cwo_Type
-#define Type_register(n,b,o) cwo_Type_register(n,b,o)
-#define Type_destroyInstance(s,i) cwo_Type_destroyInstance(s,i);
+#define Method_create cwo_Method_create
+#define Method_destroy cwo_Method_destroy
+#define Property_create cwo_Property_create
+#define Property_destroy cwo_Property_destroy
+#define Type_register cwo_Type_register
+#define Type_destroyInstance cwo_Type_destroyInstance
 #endif
 
 #endif
--- src/cwo/type.c
+++ src/cwo/type.c
@@ -3,22 +3,24 @@
 #include "internal/hashtbl.h"
 #include "internal/errors.h"
 
+#include <stdarg.h>
+
 struct cwo_Method_s
 {
     cwo_Object object;
-    cwo_String *name;
-    int (*call)();
+    const cwo_String *name;
+    cwo_MethodCall call;
     int numArgs;
-    cwo_Type **argTypes;
+    const cwo_Type *argTypes[1];
 };
 
 struct cwo_Property_s
 {
     cwo_Object object;
-    cwo_String *name;
-    void *(*get)(void);
-    void (*set)(void *);
-    cwo_Type *type;
+    const cwo_String *name;
+    cwo_PropertyGet get;
+    cwo_PropertySet set;
+    const cwo_Type *type;
 };
 
 struct cwo_TypeDescriptor_s
@@ -40,6 +42,11 @@ struct cwo_Type_s
 
 static cwoint_Hashtbl *types = 0;
 
+static const cwo_Type *t_method = 0;
+static const cwo_Type *t_property = 0;
+static const cwo_Type *t_typeDescriptor = 0;
+static const cwo_Type *t_type = 0;
+
 static void
 destroyType(void *type)
 {
@@ -55,47 +62,135 @@ static int
 init(void)
 {
     int err;
-    cwo_Type *t_object = 0;
-    cwo_Type *t_type = 0;
-    cwo_Type *t_typeDescriptor = 0;
-    cwo_Type *t_method = 0;
-    cwo_Type *t_property = 0;
-    cwo_Type *t_string = 0;
+    cwo_Type *lt_object = 0;
+    cwo_Type *lt_type = 0;
+    cwo_Type *lt_typeDescriptor = 0;
+    cwo_Type *lt_method = 0;
+    cwo_Type *lt_property = 0;
+    cwo_Type *lt_string = 0;
 
     err = cwoint_Hashtbl_create(&types, CWOINT_HASHTBL_256, &destroyType);
     if (err) return err;
     
-    err = cwo_Object_createEmpty(&t_object, sizeof(cwo_Type));
+    err = cwo_Object_createEmpty(&lt_object, sizeof(cwo_Type));
     if (err) goto s_c_type_init_fail;
 
-    err = cwo_Object_createEmpty(&t_type, sizeof(cwo_Type));
+    err = cwo_Object_createEmpty(&lt_type, sizeof(cwo_Type));
     if (err) goto s_c_type_init_fail;
 
-    err = cwo_Object_createEmpty(&t_typeDescriptor, sizeof(cwo_Type));
+    err = cwo_Object_createEmpty(&lt_typeDescriptor, sizeof(cwo_Type));
     if (err) goto s_c_type_init_fail;
 
-    err = cwo_Object_createEmpty(&t_method, sizeof(cwo_Type));
+    err = cwo_Object_createEmpty(&lt_method, sizeof(cwo_Type));
     if (err) goto s_c_type_init_fail;
 
-    err = cwo_Object_createEmpty(&t_property, sizeof(cwo_Type));
+    err = cwo_Object_createEmpty(&lt_property, sizeof(cwo_Type));
     if (err) goto s_c_type_init_fail;
 
-    err = cwo_Object_createEmpty(&t_string, sizeof(cwo_Type));
+    err = cwo_Object_createEmpty(&lt_string, sizeof(cwo_Type));
     if (err) goto s_c_type_init_fail;
 
-    err = cwo_String_create(&(t_object->name), "cwo_object");
+    /* inject type of root object */
+    err = cwo_Object_setType((void *)cwo_Object_instance(), lt_object);
+    if (err) goto s_c_type_init_fail;
+
+    /* set our types */
+    t_method = lt_method;
+    t_property = lt_property;
+    t_typeDescriptor = lt_typeDescriptor;
+    t_type = lt_type;
+
+    err = cwo_String_create(&(lt_object->name), "cwo_object");
     if (err) goto s_c_type_init_fail;
 
 s_c_type_init_fail:
-    destroyType(t_string);
-    destroyType(t_property);
-    destroyType(t_method);
-    destroyType(t_typeDescriptor);
-    destroyType(t_type);
-    destroyType(t_object);
+    destroyType(lt_string);
+    destroyType(lt_property);
+    destroyType(lt_method);
+    destroyType(lt_typeDescriptor);
+    destroyType(lt_type);
+    destroyType(lt_object);
     cwoint_Hashtbl_destroy(types);
     types = 0;
     return err;
+}
+
+SOEXPORT int
+cwo_Method_create(cwo_Method **self, const cwo_String *name,
+	cwo_MethodCall call, int numArgs, ...)
+{
+    va_list ap;
+    int err;
+    int i;
+
+    if (numArgs < 0) return CWOERR_INVARG;
+
+    err = cwo_Object_create(self, sizeof(cwo_Method) - sizeof(cwo_Type *)
+	    + (size_t)numArgs * sizeof(cwo_Type *),
+	    t_method, cwo_Object_instance());
+    if (err) return err;
+
+    (*self)->name = name;
+    (*self)->call = call;
+    (*self)->numArgs = numArgs;
+
+    va_start(ap, numArgs);
+    for (i = 0; i < numArgs; ++i)
+    {
+	(*self)->argTypes[i] = va_arg(ap, const cwo_Type *);
+    }
+    va_end(ap);
+
+    return CWO_SUCCESS;
+}
+
+SOEXPORT void
+cwo_Method_destroy(cwo_Method *self)
+{
+    free(self);
+}
+
+SOEXPORT int
+cwo_Property_create(cwo_Property **self, const cwo_String *name,
+	const cwo_Type *type, cwo_PropertyGet get, cwo_PropertySet set)
+{
+    int err;
+
+    err = cwo_Object_create(self, sizeof(cwo_Property),
+	    t_property, cwo_Object_instance());
+    if (err) return err;
+
+    (*self)->name = name;
+    (*self)->type = type;
+    (*self)->get = get;
+    (*self)->set = set;
+
+    return CWO_SUCCESS;
+}
+
+SOEXPORT void
+cwo_Property_destroy(cwo_Property *self)
+{
+    free(self);
+}
+
+SOEXPORT int
+cwo_TypeDescriptor_create(cwo_TypeDescriptor **self)
+{
+    int err;
+    err = cwo_Object_create(self, sizeof(cwo_TypeDescriptor),
+	    t_typeDescriptor, cwo_Object_instance());
+    if (err) return err;
+
+    err = cwoint_Hashtbl_create(&((*self)->methods), CWOINT_HASHTBL_16,
+	    (cwo_Destructor)&cwo_Method_destroy);
+    if (err) return err;
+
+    err = cwoint_Hashtbl_create(&((*self)->properties), CWOINT_HASHTBL_16,
+	    (cwo_Destructor)&cwo_Property_destroy);
+    if (err) return err;
+
+    return CWO_SUCCESS;
 }
 
 SOEXPORT int
