2facc264f3a1320134ec0ac2673ad91fbef59318
Felix Palmen <felix@palmen-it.de>
2015-07-14 22:14:11 +0200

    concept for OO error handling

---
+++ include/cwo/error.h
@@ -0,0 +1,48 @@
+#ifndef CWO_ERROR_H
+#define CWO_ERROR_H
+
+#include <cwo/common>
+
+struct cwo_Error_s;
+typedef struct cwo_Error_s cwo_Error;
+
+DECLEXPORT cwo_Error *
+cwo_Error_create(cwo_Error **self, int num, const char *message,
+	const char *file, unsigned long *line, const char *func);
+
+DECLEXPORT cwo_Error *
+cwo_Error_addFrame(cwo_Error *self,
+	const char *file, unsigned long *line, const char *func);
+
+DECLEXPORT void
+cwo_Error_destroy(cwo_Error *self);
+
+#define CWO_ERR_BEGIN(num, msg) do { \
+    cwo_Error *err; \
+    cwo_Error_create(*err, (num), (msg), __FILE__, __LINE__, FUNCNAME)
+
+#define CWO_ERR_END return err; } while (0)
+
+#define CWO_ERR(num, msg) CWO_ERR_BEGIN(num, msg); CWO_ERR_END
+
+#define CWO_ERRUP_BEGIN(err) do { \
+    if ((err)) { \
+	cwo_Error_addFrame((err), __FILE__, __LINE__, FUNCNAME)
+
+#define CWO_ERRUP_END() return err; } } while (0)
+
+#define CWO_ERRUP CWO_ERRUP_BEGIN(); CWO_ERRUP_END
+
+#ifdef CWO_NSIMPORT
+#define Error cwo_Error
+#define Error_create cwo_Error_create
+#define Error_addFrame cwo_Error_addFrame
+#define Error_destroy cwo_Error_destroy
+#define ERR_BEGIN CWO_ERR_BEGIN
+#define ERR_END CWO_ERR_END
+#define ERR CWO_ERR
+#define ERRUP_BEGIN CWO_ERRUP_BEGIN
+#define ERRUP_END CWO_ERRUP_END
+#define ERRUP CWO_ERR
+#endif
+
